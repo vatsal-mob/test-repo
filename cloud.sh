be43379c961c71cd9304e2fecbfbda284a2807c1
Vatsal <41470995+vatsal-mob@users.noreply.github.com>
2023-12-14 16:26:24 +0530

    Create cloud.sh
    cloudflare connect

---
+++ cloud.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiYzkzOTc4ODI3M2IzMjI2NDhmYzA0NjQ3N2UyYjU0NTYiLCJ0IjoiZDc5YjljODAtNmRhZS00MzczLTg1MDMtOTY4OWY1NWIxNTY5IiwicyI6Ik1HUTVPR1EzTjJNdE5EazJOeTAwWVRObUxUZ3dNV0l0TkRkaFpERXlaVEZpT1daaCJ9
